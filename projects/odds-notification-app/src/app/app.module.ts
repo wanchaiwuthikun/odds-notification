b25ba5b20e54bd193a312b8c3adca3ea6f3333f2
Wanchai Wuthikun <wanchai@odds.team>
2019-12-17 15:49:54 +0700

    import lib to component

--- projects/odds-notification-app/src/app/app.module.ts
+++ projects/odds-notification-app/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OddsNotificationModule } from '../../../odds-notification/src/lib/odds-notification.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, OddsNotificationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
